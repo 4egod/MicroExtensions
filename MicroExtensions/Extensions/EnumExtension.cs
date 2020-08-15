e7b6b05d01b8d967c1f539a3954b872e965a382d
Dmitry Tarasov <dmitry.tarasov@live.com>
2020-08-15 12:43:47 +0500

    feat: add Enum and IList extensions

---
+++ MicroExtensions/Extensions/EnumExtension.cs
@@ -0,0 +1,31 @@
+﻿#if MF || TINYCLR
+
+using System;
+
+public static class EnumExtension
+{
+    public static bool HasFlag(this Enum @enum, Enum flag)
+    {
+        if (flag == null)
+        {
+            throw new ArgumentNullException("flag");
+        }
+
+        if (@enum.GetType() != flag.GetType())
+        {
+            throw new ArgumentException("flag");
+        }
+
+        int i = (int)@enum.GetType().GetField("value__").GetValue(@enum);
+        int j = (int)@flag.GetType().GetField("value__").GetValue(flag);
+
+        //if ((i != 0) && (j == 0))
+        //{
+        //    return false;
+        //}
+
+        return (i & j) == j;
+    }
+}
+
+#endif
---
+++ MicroExtensions/Extensions/IListExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+public static class IListExtension
+{
+    public static bool AreElementsEqual(this IList instance, IList value, int offset, int length)
+    {
+        try
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (!instance[offset + i].Equals(value[offset + i]))
+                {
+                    return false;
+                }
+            }
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
